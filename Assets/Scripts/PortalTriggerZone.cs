39b12bac5d5ce3b0e9100377a17d54e8cbdd1c2b
YSbookcase <mokos6283@gmail.com>
2025-06-16 09:07:53 +0900

    [Chore] 기본이 되는 패턴과 과련된 스크립트 임포트

--- Assets/Scripts/PortalTriggerZone.cs
+++ Assets/Scripts/PortalTriggerZone.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class PortalTriggerZone : MonoBehaviour
 {
-    [SerializeField] private Portal parentPortal;
+    [SerializeField] private Portal1 parentPortal;
 
     private void OnTriggerEnter(Collider other)
     {
